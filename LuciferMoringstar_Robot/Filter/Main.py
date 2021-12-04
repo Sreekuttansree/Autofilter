319380bb2ec3f91b0953a6a37072531f26cb6b30
Sreekuttansree <88297456+Sreekuttansree@users.noreply.github.com>
2021-12-04 20:55:14 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -115,7 +115,7 @@ async def group(client, message):
     if 2 < len(message.text) < 50:    
         btn = []
         search = message.text
-        mo_tech_yt = text= f"**🗂️ Title:** {search}Results For Your Query: <code>{query}</code>",{message.chat.title}**"
+        mo_tech_yt = text= f"**🗂️ Title:**{search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded by{message.chat.title}**"
         nyva=BOT.get("username")
         if not nyva:
             botusername=await client.get_me()
