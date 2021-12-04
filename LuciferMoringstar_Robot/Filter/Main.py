cb644784e00d4d252ef91f7420f3d173b51ab717
Sreekuttansree <88297456+Sreekuttansree@users.noreply.github.com>
2021-12-04 20:45:43 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -116,7 +116,7 @@ async def group(client, message):
         btn = []
         search = message.text
         mo_tech_yt = text= f"**🗂️ Title:** {search}\n**⭐ Rating:** {random.choice(RATING)}\n**🎭 Genre:** {random.choice(GENRES)}\n**📤 Uploaded by {message.chat.title}**"
-        nyva=BOT.get("userna
+        nyva=BOT.get("username")
         if not nyva:
             botusername=await client.get_me()
             nyva=botusername.username
